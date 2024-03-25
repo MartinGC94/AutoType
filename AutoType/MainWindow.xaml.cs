09157c824636ea78df8d0a9092ff33f28faf8879
MartinGC94 <MGC94@outlook.dk>
2024-03-25 02:01:37 +0100

    Initial commit

---
+++ AutoType/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace AutoType
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool shouldType = true;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TypeAll_Click(object sender, RoutedEventArgs e)
+        {
+            BeginTyping(textBox.Text);
+        }
+
+        private void TypeSelection_Click(object sender, RoutedEventArgs e)
+        {
+            BeginTyping(textBox.SelectedText);
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            shouldType = false;
+        }
+
+        private void BeginTyping(string text)
+        {
+            shouldType = true;
+            _ = ThreadPool.QueueUserWorkItem(TypeText, text);
+        }
+
+        private void TypeText(object textToType)
+        {
+            IEnumerator<string> inputEnumerator = ((string)textToType).GetKeyStrokes().GetEnumerator();
+            Thread.Sleep(1000);
+            while (shouldType && inputEnumerator.MoveNext())
+            {
+                SendKeys.SendWait(inputEnumerator.Current);
+                Thread.Sleep(40);
+            }
+
+            inputEnumerator.Dispose();
+        }
+
+        private void Window_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case System.Windows.Input.Key.F5:
+                    TypeAll_Click(null, null);
+                    break;
+                case System.Windows.Input.Key.F8:
+                    TypeSelection_Click(null, null);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ AutoType/Utils.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+namespace AutoType
+{
+    internal static class Utils
+    {
+        internal static IEnumerable<string> GetKeyStrokes(this string text)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                char c = text[i];
+                string result;
+                switch (c)
+                {
+                    case '+':
+                    case '^':
+                    case '%':
+                    case '~':
+                    case '(':
+                    case ')':
+                    case '{':
+                    case '}':
+                        result = $"{{{c}}}";
+                        break;
+
+                    case '\t':
+                        result = "{TAB}";
+                        break;
+
+                    case '\n':
+                        result = "{ENTER}";
+                        break;
+
+                    case '\r':
+                        int nextCharIndex = i + 1;
+                        if (nextCharIndex != text.Length && text[nextCharIndex] == '\n')
+                        {
+                            // \r\n counts as 1 keystroke, so skip the next char.
+                            i++;
+                        }
+
+                        result = "{ENTER}";
+                        break;
+
+                    default:
+                        result = c.ToString();
+                        break;
+                }
+
+                yield return result;
+            }
+        }
+    }
+}
